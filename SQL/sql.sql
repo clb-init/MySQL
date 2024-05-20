4f5e8f02b9b5140a60caca05bda37f6aaeecd4dd
Ceo-Sammarco <noah.kd@gmail.com>
2024-05-20 20:50:06 -0300

    @Ceo-Sammarco - seg 20 mai 24

--- SQL/sql.sql
+++ SQL/sql.sql
@@ -8,9 +8,16 @@
 
 -- [ create table ]
 -- == == == == == == == == ==
+create table `Employees` (
+   
+)
 
 -- [ insert data into a table ]
 -- == == == == == == == == ==
 
 -- [ select ] 
 -- == == == == == == == == ==
+-- all
+select * from schemaName.tableName;
+
+-- 
