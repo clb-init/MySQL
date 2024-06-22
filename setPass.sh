7626ff2e3141fa3c884021d362ce39b961311516
Ceo-Sammarco <noah.kd@gmail.com>
2024-06-22 16:46:12 -0300

    @Ceo-Sammarco - sáb 22 jun 24

--- setPass.sh
+++ setPass.sh
@@ -8,15 +8,15 @@ echo
 
 if [ ! $pass1 = $pass2 ]
 then
-        echo "Sorry, passwords do not match."
+   echo "Sorry, passwords do not match."
 else
-        echo -e "alter user root@localhost identified by \"${pass1}\"" > setpass.sql
-        mysql mysql < setpass.sql > mysqllogs.log &&
-        echo "New password was successfully set." ||
-        echo "Failed to set new password"
-        rm setpass.sql
-        if [ ! -s mysqllogs.log ]
-        then
-                rm mysqllogs.log
-        fi
+   echo -e "alter user root@localhost identified by \"${pass1}\"" > setpass.sql
+   mysql mysql < setpass.sql > mysqllogs.log &&
+   echo "New password was successfully set." ||
+   echo "Failed to set new password"
+   rm setpass.sql
+   if [ ! -s mysqllogs.log ]
+   then
+      rm mysqllogs.log
+   fi
 fi
